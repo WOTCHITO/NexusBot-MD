40bb7f3f0ff405296b871b9e7e1ca17890f11b8e
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2025-07-22 21:46:30 -0500

    Update _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -5,7 +5,7 @@ if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.antifake) {
 let texto = `᥀·࣭࣪̇˖✨️◗ 𝙃𝙤𝙡𝙖 *@${m.sender.split`@`[0]}*, 𝙡𝙤𝙨 𝙣𝙪𝙢𝙚𝙧𝙤𝙨 𝙧𝙖𝙧𝙤𝙨 𝙣𝙤 𝙚𝙨𝙩𝙖𝙣 𝙥𝙚𝙧𝙢𝙞𝙩𝙞𝙙𝙤𝙨 𝙖𝙦𝙪𝙞, 𝙨𝙚𝙧𝙖𝙨 𝙚𝙡𝙞𝙢𝙞𝙣𝙖𝙙𝙤.`
-if (m.sender.startsWith('1' || '61')) {
+if (m.sender.startsWith('61' || '61')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
